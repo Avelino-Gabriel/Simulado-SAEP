666a8bb31088119fec5e55fd1da85799d2bcbac1
Avelino-Gabriel <aluno103.23187221gabriel@gmail.com>
2024-04-15 21:06:39 -0300

    Criação Models

--- acesso/models.py
+++ acesso/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Sexo(models.Model):
+    sexo = models.CharField(max_length=256)
+class Professor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=11)
+    rg = models.CharField(max_length=9)
+    data_nascimento = models.DateField()
+    sexo = models.OneToOneField(Sexo, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.user.username
+
+class Turma(models.Model):
+    nome = models.CharField(max_length=256)
+    periodo = models.CharField(max_length=256)
+    id_professor = models.ForeignKey(Professor, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
+
+class Atividade(models.Model):
+    titulo = models.CharField(max_length=256)
+    descricao = models.TextField()
+    id_turma = models.ForeignKey(Turma, on_delete=models.DO_NOTHING)
+    id_professor = models.OneToOneField(Professor, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.titulo
